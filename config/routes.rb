fed03102734f6bd6e0d401f4290b34e769274701
Ron Bacalla <wronfreek@gmail.com>
2014-12-17 15:12:10 +0900

    demo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Wronfreek::Application.routes.draw do
+  match '/', to: 'welcome#index', via: :all
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
