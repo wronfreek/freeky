f6cfe26cae9172860b7eb3e96c992365389a1c92
Ron Bacalla <wronfreek@gmail.com>
2014-12-17 11:37:32 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wronfreek::Application.config.secret_key_base = '44feb7d1b1dc27513020854aa4abd81d48406bbaf530a5e54f24d076aac36a7dc8dce8a22c51badf1aad37c03b2aaba5e88a13dae5f2592d68be85f8670d4c1b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wronfreek::Application.config.session_store :cookie_store, key: '_wronfreek_session'
